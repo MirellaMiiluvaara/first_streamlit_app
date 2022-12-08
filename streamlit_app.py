bb58ac41e76fa2d111b0fb3456e085fe1b1b243a
Mirella <64734402+MirellaMiiluvaara@users.noreply.github.com>
2022-12-08 15:00:43 +0200

    Update streamlit_app.py
    Added header
    Added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast menu')
+streamit.text('Blueberry porrige')
+streamit.text('Smoothie')
+streamit.text('Omelet')
